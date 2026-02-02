e315f8536da8b59f1822943b4ca6665be65733a5
Kreedyy <kreedyhaj@gmail.com>
2026-02-02 02:13:43 +0100

    Add sync route and icon

--- src/lib/assets/GetStarted.svelte
+++ src/lib/assets/GetStarted.svelte
@@ -25,12 +25,13 @@
 		<p>3.</p>
 		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640"
 			><!--!Font Awesome Free v7.1.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2026 Fonticons, Inc.--><path
-				d="M416.9 85.2L372 130.1L509.9 268L554.8 223.1C568.4 209.6 576 191.2 576 172C576 152.8 568.4 134.4 554.8 120.9L519.1 85.2C505.6 71.6 487.2 64 468 64C448.8 64 430.4 71.6 416.9 85.2zM338.1 164L122.9 379.1C112.2 389.8 104.4 403.2 100.3 417.8L64.9 545.6C62.6 553.9 64.9 562.9 71.1 569C77.3 575.1 86.2 577.5 94.5 575.2L222.3 539.7C236.9 535.6 250.2 527.9 261 517.1L476 301.9L338.1 164z"
+				d="M384 128C384 145.7 369.7 160 352 160L128 160C110.3 160 96 145.7 96 128C96 110.3 110.3 96 128 96L352 96C369.7 96 384 110.3 384 128zM384 384C384 401.7 369.7 416 352 416L128 416C110.3 416 96 401.7 96 384C96 366.3 110.3 352 128 352L352 352C369.7 352 384 366.3 384 384zM96 256C96 238.3 110.3 224 128 224L512 224C529.7 224 544 238.3 544 256C544 273.7 529.7 288 512 288L128 288C110.3 288 96 273.7 96 256zM544 512C544 529.7 529.7 544 512 544L128 544C110.3 544 96 529.7 96 512C96 494.3 110.3 480 128 480L512 480C529.7 480 544 494.3 544 512z"
 			/></svg
 		>
 		<p>Start syncing lyrics</p>
 	</div>
-	<a href="/"><!--Link to example video placed in static/assets-->
+	<a href="/"
+		><!--Link to example video placed in static/assets-->
 		<p class="tip">
 			TIP: Sync lines preemptively, sync close to end<br /> of last word being sung BEFORE next line starts
 		</p>
