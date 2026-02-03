72118382c9d4fb15127f35ac917ee4cbe5f2b89d
Kreedyy <kreedyhaj@gmail.com>
2026-02-03 19:36:31 +0100

    Make results show

--- src/lib/assets/ShowResults.svelte
+++ src/lib/assets/ShowResults.svelte
@@ -7,7 +7,33 @@
 		userPick: any;
 	} = $props<{}>();
 
-	//$effect(() => {
-	//	console.log(data);
-	//});
+	$effect(() => {
+		console.log(data);
+	});
 </script>
+
+<!--
+.trackName string
+.name string //Same as .trackName?
+.albumName string
+.artistName string
+.duration number
+.id number
+.instrumental bool
+.plainLyrics string
+-->
+
+<div class="container">
+	{#each data as result}
+		<div class="result">
+			<p>{result.trackName}</p>
+		</div>
+	{/each}
+</div>
+
+<style>
+	.container{
+		display: flex;
+		flex-direction: column;
+	}
+</style>
